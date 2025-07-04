a6058d812c85d784b1553867bf1be0cf2a809b4d
ZihadHossainNayem <zihadhossainnayem@gmail.com>
2025-07-05 00:28:37 +0600

    engine : function for chess position conversion

--- engine/gameEngine.js
+++ engine/gameEngine.js
@@ -17,3 +17,20 @@ export const showBoard = (board) => {
     console.log(row.join(" | "));
   }
 };
+
+export const algebraicToIndex = (position) => {
+  const colChar = position[0];
+  const rowChar = position[1];
+
+  const col = colChar.charCodeAt(0) - "a".charCodeAt(0); // 'a'-'h' to 0-7
+  const row = 8 - parseInt(rowChar); // '1'-'8' to 7-0
+
+  return [row, col];
+};
+
+export const indexToAlgebraic = (row, col) => {
+  const colChar = String.fromCharCode("a".charCodeAt(0) + col); // 0-7 to 'a'-'h'
+  const rowChar = 8 - row; // 7-0 to '1'-'8'
+
+  return colChar + rowChar;
+};
--- engine/gameEngineTest.js
+++ engine/gameEngineTest.js
@@ -8,3 +8,6 @@ import {
 
 const board = initBoardPosition();
 showBoard(board);
+
+console.log(algebraicToIndex("e2"));
+console.log(indexToAlgebraic(6, 4));
