7c0a61cc521151f92f2690b5bfa12314cd05b5d4
ZihadHossainNayem <zihadhossainnayem@gmail.com>
2025-07-05 11:46:49 +0600

    engine : function for legal rook move; error check on common functions

--- engine/gameEngine.js
+++ engine/gameEngine.js
@@ -1,92 +1,155 @@
 export const initBoardPosition = () => {
-  return [
-    ["bRK", "bKN", "bBP", "bQN", "bKG", "bBP", "bKN", "bRK"], // black piece
-    ["bPN", "bPN", "bPN", "bPN", "bPN", "bPN", "bPN", "bPN"], // black piece
-    ["   ", "   ", "   ", "   ", "   ", "   ", "   ", "   "],
-    ["   ", "   ", "   ", "   ", "   ", "   ", "   ", "   "],
-    ["   ", "   ", "   ", "   ", "   ", "   ", "   ", "   "],
-    ["   ", "   ", "   ", "   ", "   ", "   ", "   ", "   "],
-    ["wPN", "wPN", "wPN", "wPN", "wPN", "wPN", "wPN", "wPN"], // white piece
-    ["wRK", "wKN", "wBP", "wQN", "wKG", "wBP", "wKN", "wRK"], // white piece
-  ];
+    return [
+        ['bRK', 'bKN', 'bBS', 'bQN', 'bKG', 'bBS', 'bKN', 'bRK'], // black piece
+        ['bPN', 'bPN', 'bPN', 'bPN', 'bPN', 'bPN', 'bPN', 'bPN'], // black piece
+        ['   ', '   ', '   ', '   ', '   ', '   ', '   ', '   '],
+        ['   ', '   ', '   ', '   ', '   ', '   ', '   ', '   '],
+        ['   ', '   ', '   ', '   ', '   ', '   ', '   ', '   '],
+        ['   ', '   ', '   ', '   ', '   ', '   ', '   ', '   '],
+        ['wPN', 'wPN', 'wPN', 'wPN', 'wPN', 'wPN', 'wPN', 'wPN'], // white piece
+        ['wRK', 'wKN', 'wBS', 'wQN', 'wKG', 'wBS', 'wKN', 'wRK'], // white piece
+    ];
 };
 
 export const showBoard = (board) => {
-  console.log("\ncurrent board:");
-  for (let row of board) {
-    console.log(row.join(" | "));
-  }
+    console.log('\ncurrent board:');
+    for (let row of board) {
+        console.log(row.join(' | '));
+    }
 };
 
 export const algebraicToIndex = (position) => {
-  const colChar = position[0];
-  const rowChar = position[1];
+    if (!position || position.length !== 2) {
+        throw new Error('invalid position format');
+    }
 
-  const col = colChar.charCodeAt(0) - "a".charCodeAt(0); // 'a'-'h' to 0-7
-  const row = 8 - parseInt(rowChar); // '1'-'8' to 7-0
+    const colChar = position[0];
+    const rowChar = position[1];
 
-  return [row, col];
+    if (colChar < 'a' || colChar > 'h' || rowChar < '1' || rowChar > '8') {
+        throw new Error('position out of bound');
+    }
+
+    const col = colChar.charCodeAt(0) - 'a'.charCodeAt(0); // 'a'-'h' to 0-7
+    const row = 8 - parseInt(rowChar); // '1'-'8' to 7-0
+
+    return [row, col];
 };
 
 export const indexToAlgebraic = (row, col) => {
-  const colChar = String.fromCharCode("a".charCodeAt(0) + col); // 0-7 to 'a'-'h'
-  const rowChar = 8 - row; // 7-0 to '1'-'8'
+    if (row < 0 || row > 7 || col < 0 || col > 7) {
+        throw new Error('index out of bound');
+    }
 
-  return colChar + rowChar;
+    const colChar = String.fromCharCode('a'.charCodeAt(0) + col); // 0-7 to 'a'-'h'
+    const rowChar = 8 - row; // 7-0 to '1'-'8'
+
+    return colChar + rowChar;
 };
 
 export const movePiece = (board, from, to) => {
-  const [fromRow, fromCol] = algebraicToIndex(from);
-  const [toRow, toCol] = algebraicToIndex(to);
+    const [fromRow, fromCol] = algebraicToIndex(from);
+    const [toRow, toCol] = algebraicToIndex(to);
 
-  // piece at the starting position
-  const piece = board[fromRow][fromCol];
+    // piece at the starting position
+    const piece = board[fromRow][fromCol];
+    if (piece === '   ') {
+        throw new Error('no piece at starting position');
+    }
 
-  // piece to new position
-  board[toRow][toCol] = piece;
+    // piece to new position
+    board[toRow][toCol] = piece;
 
-  // clear its previous position
-  board[fromRow][fromCol] = "   ";
+    // clear its previous position
+    board[fromRow][fromCol] = '   ';
 
-  return board;
+    return board;
 };
 
 export const isPawnMoveLegal = (board, from, to) => {
-  const [fromRow, fromCol] = algebraicToIndex(from);
-  const [toRow, toCol] = algebraicToIndex(to);
+    const [fromRow, fromCol] = algebraicToIndex(from);
+    const [toRow, toCol] = algebraicToIndex(to);
 
-  const piece = board[fromRow][fromCol];
-  const color = piece[0]; // 'w' or 'b'
+    const piece = board[fromRow][fromCol];
+    if (piece === '   ' || piece.substring(1) !== 'PN') {
+        return false;
+    }
+    const color = piece[0]; // 'w' or 'b'
 
-  const direction = color === "w" ? -1 : 1; // white move up -1, black moves down +1
-  const startRow = color === "w" ? 6 : 1;
+    const direction = color === 'w' ? -1 : 1; // white move up -1, black moves down +1
+    const startRow = color === 'w' ? 6 : 1;
 
-  const diffRow = toRow - fromRow;
-  const diffCol = toCol - fromCol;
+    const diffRow = toRow - fromRow;
+    const diffCol = toCol - fromCol;
 
-  // pawn normal move: 1 step forward, (to) must be empty
-  if (diffCol === 0 && diffRow === direction && board[toRow][toCol] === "   ") {
-    return true;
-  }
+    // pawn normal move: 1 step forward, (to) must be empty
+    if (diffCol === 0 && diffRow === direction && board[toRow][toCol] === '   ') {
+        return true;
+    }
 
-  // first pawn move: 2 steps forward
-  if (
-    diffCol === 0 &&
-    diffRow === 2 * direction &&
-    fromRow === startRow &&
-    board[fromRow + direction][fromCol] === "   " &&
-    board[toRow][toCol] === "   "
-  ) {
-    return true;
-  }
+    // first pawn move: 2 steps forward
+    if (
+        diffCol === 0 &&
+        diffRow === 2 * direction &&
+        fromRow === startRow &&
+        board[fromRow + direction][fromCol] === '   ' &&
+        board[toRow][toCol] === '   '
+    ) {
+        return true;
+    }
 
-  // capture move: diagonal 1 step, opponent piece present
-  if (
-    Math.abs(diffCol) === 1 && // diagonal to left or right
-    diffRow === direction &&
-    board[toRow][toCol] !== "   " &&
-    board[toRow][toCol][0] !== color // opponents piece
-  ) {
-    return true;
-  }
+    // capture move: diagonal 1 step, opponent piece present
+    if (
+        Math.abs(diffCol) === 1 && // diagonal to left or right
+        diffRow === direction &&
+        board[toRow][toCol] !== '   ' &&
+        board[toRow][toCol][0] !== color // opponents piece
+    ) {
+        return true;
+    }
+
+    return false;
+};
+
+export const isRookMoveLegal = (board, from, to) => {
+    const [fromRow, fromCol] = algebraicToIndex(from);
+    const [toRow, toCol] = algebraicToIndex(to);
+
+    const piece = board[fromRow][fromCol];
+    if (piece === '   ' || piece.substring(1) !== 'RK') {
+        return false;
+    }
+    const color = piece[0];
+
+    // rook have to move in straight line
+    const isVerticalMove = fromCol === toCol && fromRow !== toRow;
+    const isHorizontalMove = fromRow === toRow && fromCol !== toCol;
+
+    if (!isVerticalMove && !isHorizontalMove) return false;
+
+    // direction of movement
+    const stepRow = Math.sign(toRow - fromRow);
+    const stepCol = Math.sign(toCol - fromCol);
+
+    // path blockage check, rook can't jump over piece
+    let row = fromRow + stepRow;
+    let col = fromCol + stepCol;
+
+    while (row !== toRow || col !== toCol) {
+        if (board[row][col] !== '   ') {
+            return false; // blocked
+        }
+        row += stepRow;
+        col += stepCol;
+    }
+
+    const targetPiece = board[toRow][toCol];
+
+    // move
+    if (targetPiece === '   ') return true;
+
+    // capture
+    if (targetPiece[0] !== color) return true;
+
+    return false;
 };
--- engine/gameEngineTest.js
+++ engine/gameEngineTest.js
@@ -1,50 +1,70 @@
 import {
-  initBoardPosition,
-  showBoard,
-  algebraicToIndex,
-  indexToAlgebraic,
-  movePiece,
-  isPawnMoveLegal,
-} from "./gameEngine.js";
+    initBoardPosition,
+    showBoard,
+    algebraicToIndex,
+    indexToAlgebraic,
+    movePiece,
+    isPawnMoveLegal,
+    isRookMoveLegal,
+} from './gameEngine.js';
 
 let board = initBoardPosition();
 showBoard(board);
 
-console.log(algebraicToIndex("e2"));
+console.log(algebraicToIndex('e2'));
 console.log(indexToAlgebraic(6, 4));
 
-console.log("\nmoving piece from d2 to d4");
-movePiece(board, "d2", "d3");
+// piece moving
+console.log('\nmoving piece from d2 to d4');
+movePiece(board, 'd2', 'd3');
 showBoard(board);
 
 board = initBoardPosition();
 showBoard(board);
 
-console.log("\ntesting pawn moves");
+console.log('\ntesting pawn moves');
 
 // pawn move forward one step
-console.log(
-  isPawnMoveLegal(board, "d2", "d3") === true ? "case 1: pass" : "case 1: fail"
-);
+console.log(isPawnMoveLegal(board, 'd2', 'd3') === true ? 'pawn case 1: pass' : 'pawn case 1: fail');
 
 // pawn move forward two steps
-console.log(
-  isPawnMoveLegal(board, "e2", "e4") === true ? "case 2: pass" : "case 2: fail"
-);
+console.log(isPawnMoveLegal(board, 'e2', 'e4') === true ? 'pawn case 2: pass' : 'pawn case 2: fail');
 
 // illegal pawn move
-console.log(
-  isPawnMoveLegal(board, "f2", "f5") == true ? "case 3: pass" : "case 4: fail"
-);
+console.log(isPawnMoveLegal(board, 'f2', 'f5') === false ? 'pawn case 3: pass' : 'pawn case 3: fail');
 
 // pawn capture move
-const [row, col] = algebraicToIndex("f3");
-board[row][col] = "bBP";
-console.log(
-  isPawnMoveLegal(board, "e2", "f3") === true ? "case 4: pass" : "case 4: fail"
-);
+movePiece(board, 'f7', 'f3');
+console.log(isPawnMoveLegal(board, 'e2', 'f3') === true ? 'pawn case 4: pass' : 'pawn case 4: fail');
 
 // invalid capture
-console.log(
-  isPawnMoveLegal(board, "e2", "d3") === true ? "case 5: pass" : "case 5: fail"
-);
+console.log(isPawnMoveLegal(board, 'e2', 'd3') === false ? 'pawn case 5: pass' : 'pawn case 5: fail');
+
+console.log('\ntesting rook moves');
+board = initBoardPosition();
+showBoard(board);
+
+movePiece(board, 'h2', 'h4');
+movePiece(board, 'h4', 'h5');
+showBoard(board);
+
+// rook moves up
+console.log(isRookMoveLegal(board, 'h1', 'h3') === true ? 'rook case 1: pass' : 'rook case 1: fail');
+
+// rook block by own piece
+console.log(isRookMoveLegal(board, 'h1', 'f1') === false ? 'rook case 2: pass' : 'rook case 2: fail');
+
+// illegal diagonal move
+console.log(isRookMoveLegal(board, 'h1', 'g2') === false ? 'rook case 3: pass' : 'rook case 3:fail');
+
+// capture test
+movePiece(board, 'h1', 'h3');
+movePiece(board, 'g8', 'h6');
+movePiece(board, 'h5', 'g3');
+showBoard(board);
+console.log(isRookMoveLegal(board, 'h3', 'h6') === true ? 'rook case 4: pass' : 'rook case 4: fail');
+
+// invalid capture test
+movePiece(board, 'f1', 'h7');
+showBoard(board);
+console.log(isRookMoveLegal(board, 'h3', 'h7') === false ? 'rook case 5: pass' : 'rook case 5: fail');
